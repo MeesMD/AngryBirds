809fce40edbdf9f60b2d2ce69acf573525f067dd
MeesMD <meesmilan@hotmail.com>
2018-11-15 13:35:03 +0100

    Add files via upload
    Initial Commit.

---
+++ Assets/Scripts/DestroyerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyerScript : MonoBehaviour {
+
+    public float forceNeeded;
+
+    float collisionForce(Collision2D collider)
+    {
+        // Estimate a collision's force (speed * mass)
+        float speed = collider.relativeVelocity.sqrMagnitude;
+        if (collider.collider.GetComponent<Rigidbody2D>())
+        {
+            return speed * collider.collider.GetComponent<Rigidbody2D>().mass;
+        } 
+        return speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collisionForce(collision) > forceNeeded)
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PullAndRelease.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PullAndRelease : MonoBehaviour {
+ 
+    Vector2 startPos;
+    public float force = 1300;
+
+   
+    void Start ()
+    {
+        startPos = transform.position;    
+    }
+
+    void OnMouseUp()
+    { 
+        GetComponent<Rigidbody2D>().isKinematic = false;
+       
+        Vector2 dir = startPos - (Vector2)transform.position;
+        GetComponent<Rigidbody2D>().AddForce(dir * force);
+        Destroy(this);
+    }
+
+    void OnMouseDrag()
+    {        
+        
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        float radius = 1.8f;
+        Vector2 dir = pos - startPos;
+        if (dir.sqrMagnitude > radius)
+        {
+            dir = dir.normalized * radius;
+        }
+        transform.position = startPos + dir;
+    }
+}
